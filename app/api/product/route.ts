d16ca625836d871c9dfaec9e9c6ca4f6de4926fa
Fabian <acostahf4@gmail.com>
2024-01-12 15:24:20 -0600

    update func for product info

--- app/api/product/route.ts
+++ app/api/product/route.ts
@@ -1,4 +1,4 @@
-import { getFirestore, getDoc, doc } from "firebase/firestore";
+import { getFirestore, getDoc, doc, updateDoc } from "firebase/firestore";
 import { NextResponse } from "next/server";
 import app from "../../../firebase";
 import { getServerSession } from "next-auth/next";
@@ -25,5 +25,42 @@ export async function GET(request: Request) {
 		return NextResponse.json(data);
 	} catch (error) {
 		return NextResponse.json({ error: error });
+	}
+}
+
+export async function PUT(request: Request) {
+	const session = await getServerSession(authOptions);
+	const user = session?.user;
+
+	// Ensure that the user is authenticated
+	if (!user) {
+		return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+	}
+
+	const url = new URL(request.url);
+	const id = url.searchParams.get("id");
+	const docRef = doc(
+		db,
+		"users",
+		user.email as string,
+		"products",
+		id as string
+	);
+
+	try {
+		const data = await request.json();
+		const { title, ebayLink, aiTitle, aiDescription } = data;
+
+		// Update the document with the new data
+		await updateDoc(docRef, {
+			title,
+			ebayLink,
+			aiTitle,
+			aiDescription,
+		});
+
+		return NextResponse.json({ message: "Document updated successfully" });
+	} catch (error) {
+		return NextResponse.json({ error: error.message }, { status: 500 });
 	}
 }
--- app/product/[...slug]/page.tsx
+++ app/product/[...slug]/page.tsx
@@ -1,12 +1,16 @@
 "use client";
 import { Product } from "@/types";
 import { useEffect, useState } from "react";
-import { Divider } from "@nextui-org/react";
+import { Divider, Input } from "@nextui-org/react";
 import CopyCard from "@/components/cards/CopyCard";
 import { Button } from "@nextui-org/button";
 
 export default function Page({ params }: { params: { slug: string } }) {
 	const [product, setProduct] = useState<Product | null>(null);
+	const [editableTitle, setEditableTitle] = useState("");
+	const [editableEbayLink, setEditableEbayLink] = useState("");
+	const [aiTitle, setAiTitle] = useState("");
+	const [aiDescription, setAiDescription] = useState("");
 
 	useEffect(() => {
 		try {
@@ -30,41 +34,76 @@ export default function Page({ params }: { params: { slug: string } }) {
 		}
 	}, [params.slug]);
 
+	useEffect(() => {
+		if (product) {
+			setEditableTitle(product.title);
+			setEditableEbayLink(product?.ebayLink);
+			setAiTitle(product?.aiTitle);
+			setAiDescription(product?.aiDescription);
+		}
+	}, [product]);
+
+	const handleTitleChange = (e) => {
+		setEditableTitle(e.target.value);
+	};
+
+	const handleEbayLinkChange = (e) => {
+		setEditableEbayLink(e.target.value);
+	};
+
 	const handleUpdate = async () => {
-		//TODO: update ebay link
+		try {
+			console.log("Updating...");
+			const res = await fetch(`/api/product/?id=${params.slug}`, {
+				method: "PUT",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({
+					title: editableTitle,
+					ebayLink: editableEbayLink,
+					aiTitle: aiTitle,
+					aiDescription: aiDescription,
+				}),
+			});
+			const data = await res.json();
+			console.log(data);
+			if (data.error) {
+				throw new Error(data.error);
+			}
+		} catch (error) {
+			console.log(error);
+		}
 	};
 
 	return (
-		<div className="p-6">
-			<h1 className="text-3xl font-bold mb-4">{product?.title}</h1>
+		<div className="w-full p-6">
 			<Divider />
 			<h2 className="text-2xl mt-6 mb-4">Sources</h2>
 			<Divider />
-			<div className="flex gap-4  pt-4">
-				<a
-					href="..."
-					className="text-blue-600 hover:text-blue-800 visited:text-purple-600"
-				>
-					Ebay Link
-				</a>
+			<Input
+				className="text-3xl font-bold mb-4"
+				value={editableTitle}
+				onChange={handleTitleChange}
+			/>
+			<div className="w-full  pb-4">
+				<Input value={editableEbayLink} onChange={handleEbayLinkChange} />
+			</div>
+			<div className="w-full flex justify-end">
 				<Button onPress={handleUpdate} size="sm" color="secondary">
 					Update
 				</Button>
 			</div>
-			<p className="mt-2 mb-4">Cost of Goods: ${product?.cost}</p>
+			<p className="mt-2 mb-4">
+				Cost of Goods: ${product?.cost.toFixed(2)}
+			</p>
 			<div className="flex flex-col gap-4">
 				<div className="flex flex-col gap-2">
 					<h1 className="text-xl font-bold">Listing Content</h1>
 					<Divider />
 				</div>
-				<CopyCard
-					title="Title"
-					content="Make beautiful websites regardless of your design experience."
-				/>
-				<CopyCard
-					title="Description"
-					content="Make beautiful websites regardless of your design experience."
-				/>
+				<CopyCard title="Title" content={aiTitle} />
+				<CopyCard title="Description" content={aiDescription} />
 			</div>
 		</div>
 	);
