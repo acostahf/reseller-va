8c0d6ffcf36ab7bfbf780eca7e6dbe95b9dc82af
Fabian <acostahf4@gmail.com>
2024-01-23 12:46:00 -0600

    create lookup page

---
+++ app/lookup/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import React from "react";
+
+const lookup = async () => {
+	return (
+		<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10"></section>
+	);
+};
+
+export default lookup;
