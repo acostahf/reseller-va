2cfffbe8799dd9a0f9a7149271829a99e8e1cc6f
Fabian <acostahf4@gmail.com>
2024-03-19 16:49:31 -0500

    testing multiple request

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -14,20 +14,20 @@ export default async function DashboardPage() {
 			"X-User-Email": session?.user?.email as string,
 		},
 	});
-	const bundlesResp = await fetch("http://localhost:3000/api/bundles", {
-		method: "GET",
-		headers: {
-			"Content-Type": "application/json",
-			"X-User-Email": session?.user?.email as string,
-		},
-	});
+	// const bundlesResp = await fetch("http://localhost:3000/api/bundles", {
+	// 	method: "GET",
+	// 	headers: {
+	// 		"Content-Type": "application/json",
+	// 		"X-User-Email": session?.user?.email as string,
+	// 	},
+	// });
 
-	const bundles = await bundlesResp.json();
+	// const bundles = await bundlesResp.json();
 	const stats = await statsResp.json();
 
 	return (
 		<div className="flex flex-col gap-4 w-full justify-center items-center">
-			{/* <StatsCard data={stats} /> */}
+			<StatsCard data={stats} />
 			{/* <Inventory data={bundles} /> */}
 			<AddFinds />
 		</div>
