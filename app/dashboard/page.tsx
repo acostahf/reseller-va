f5f75292f3a605b36d42cd7b6a443f50c10ecdb8
Fabian <acostahf4@gmail.com>
2024-03-19 15:32:01 -0500

    remove dynamic vars

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,37 +3,29 @@ import AddFinds from "@/components/sections/addFinds";
 import Inventory from "@/components/sections/inventory";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
-import useAppStore from "../context/stores/appStore";
-
-type D = {
-	stats: any;
-	bundles: any;
-};
 
 const getData = async () => {
-	let d: D;
 	const session = await getServerSession(authOptions);
-	const userEmail = session?.user?.email;
+
 	const statsResp = await fetch("http://localhost:3000/api/stats", {
 		method: "GET",
 		headers: {
 			"Content-Type": "application/json",
-			"X-User-Email": userEmail as string,
+			"X-User-Email": session?.user?.email as string,
 		},
 	});
 	const bundleResp = await fetch("http://localhost:3000/api/bundles", {
 		method: "GET",
 		headers: {
 			"Content-Type": "application/json",
-			"X-User-Email": userEmail as string,
+			"X-User-Email": session?.user?.email as string,
 		},
 	});
 
 	const bundles = await bundleResp.json();
 	const stats = await statsResp.json();
 
-	d = { stats, bundles };
-	return d;
+	return { stats, bundles };
 };
 
 export default async function DashboardPage() {
