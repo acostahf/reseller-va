167690013396b89e57d371c6b1ee1362dac59ec4
Fabian <acostahf4@gmail.com>
2024-03-19 16:20:43 -0500

    fix the way I was returning the data

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -7,29 +7,28 @@ import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 const getData = async () => {
 	const session = await getServerSession(authOptions);
 
-	const statsResp = await fetch("http://localhost:3000/api/stats", {
+	const stats = await fetch("http://localhost:3000/api/stats", {
 		method: "GET",
 		headers: {
 			"Content-Type": "application/json",
 			"X-User-Email": session?.user?.email as string,
 		},
 	});
-	const bundleResp = await fetch("http://localhost:3000/api/bundles", {
+	const bundles = await fetch("http://localhost:3000/api/bundles", {
 		method: "GET",
 		headers: {
 			"Content-Type": "application/json",
 			"X-User-Email": session?.user?.email as string,
 		},
 	});
-
-	const bundles = await bundleResp.json();
-	const stats = await statsResp.json();
 
 	return { stats, bundles };
 };
 
 export default async function DashboardPage() {
-	const { stats, bundles } = await getData();
+	const data = await getData();
+	const bundles = await data.bundles.json();
+	const stats = await data.stats.json();
 
 	return (
 		<div className="flex flex-col gap-4 w-full justify-center items-center">
